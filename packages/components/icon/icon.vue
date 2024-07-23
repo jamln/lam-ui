8ee1c4fb9f03cb23522f26df5bb66a71b9a77a4b
Jam <jamln@live.com>
2024-07-23 16:30:34 +0800

    fix: 引入iconfont.js导致vitepress 构建报错问题修复

--- packages/components/icon/icon.vue
+++ packages/components/icon/icon.vue
@@ -4,10 +4,14 @@
   </svg>
 </template>
 <script setup lang="ts">
-import './font/iconfont.js'
+// import './font/iconfont.js'
 import './style/index.less'
-import { computed, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { iconProps } from './types'
+onMounted(() => {
+  //@ts-ignore
+  import('./font/iconfont.js').then(() => { });
+})
 const props = defineProps(iconProps)
 const iconName = computed(() => `#lam-${props.name}`)
 // const iconColor = ref(props.color)
